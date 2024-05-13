c196e014307748be3a689b8a11b69325181468dd
ErnestJohnDecina <89255108+ErnestDecina@users.noreply.github.com>
2024-05-13 05:21:51 +0000

    feat: problem 1 functionality complete

--- src/problem-1.py
+++ src/problem-1.py
@@ -8,8 +8,29 @@
 from nltk import word_tokenize, sent_tokenize, pos_tag
 
 sentence = "Steve Jobs was an American entrepreneur and business magnate. He was the chairman, chief executive officer (CEO), and a co-founder of Apple Inc., chairman and majority shareholder of Pixar, a member of The Walt Disney Company's board of directors following its acquisition of Pixar, and the founder, chairman, and CEO of NeXT. Jobs is widely recognized as a pioneer of the microcomputer revolution of the 1970s and 1980s, along with Apple co-founder Steve Wozniak. "
+named_entities = []
+pos_tags = {"NNP": []}
 
 # Loop through each word
-for sentence_token in sent_tokenize(sentence):
-    word_tokens = word_tokenize(sentence_token)
-    print(pos_tag(word_tokens), end="\n")
+word_tokens = word_tokenize(sentence)
+word_tokens_pos_tag = pos_tag(word_tokens)
+
+for word_token_pos_tag in word_tokens_pos_tag:
+    if word_token_pos_tag[1] == "NNP" :
+        pos_tags["NNP"].append(word_token_pos_tag[0])
+    else:
+        if word_token_pos_tag[1] not in pos_tags:
+            pos_tags[word_token_pos_tag[1]] = []
+
+        pos_tags[word_token_pos_tag[1]].append(word_token_pos_tag[0]) 
+            
+
+
+
+print("Parts of Speech Tags")
+for key in pos_tags.keys():
+    print(key, end="\t")
+    print(pos_tags[key], end="\n")
+
+
+
